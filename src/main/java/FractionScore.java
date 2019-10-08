9c79b2b54f95c2efcb7eb4da9d397d710fad1058
Gagandeep Singh <gagandeep16037@iiitd.ac.in>
2019-10-08 13:11:17 +0530

    Fraction Aggregation done

--- src/main/java/FractionScore.java
+++ src/main/java/FractionScore.java
@@ -6,11 +6,13 @@ import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.sources.In;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 import scala.Tuple2;
 import shapeless.Tuple;
 
 import java.io.Serializable;
 import java.util.*;
+import java.util.function.BiConsumer;
 
 class Spatial_Feature implements Serializable{
     private String feature_name;
@@ -74,7 +76,7 @@ public class FractionScore {
 
         return Math.sqrt(diff_x+diff_y);
     }
-    public static LinkedList<Object> find_points_in_dist_d(int d,Object point,HashMap<Object, HashMap<String,Integer>> neighbour_count_map)
+    public static LinkedList<Object> find_points_in_dist_d(double d,Object point,HashMap<Object, HashMap<String,Integer>> neighbour_count_map)
     {
         LinkedList<Object> list_of_points_in_d=new LinkedList<>();
         for(Object o:neighbour_count_map.keySet())
@@ -85,7 +87,7 @@ public class FractionScore {
             }
             else
             {
-                if(calca_dist(o,point)>=d)
+                if(calca_dist(o,point)<=d)
                 {
                     list_of_points_in_d.add(o);
                 }
@@ -94,15 +96,17 @@ public class FractionScore {
 
         return list_of_points_in_d;
     }
-    public static HashMap<Object, HashMap<String,Double>> FractionComputation(JavaRDD<Object> points_rdd,JavaRDD<String> spatial_feature_rdd, int dist_thresh)
+    public static HashMap<Object, HashMap<String,Double>> FractionComputation(JavaRDD<Object> points_rdd,JavaRDD<String> spatial_feature_rdd, double dist_thresh)
     {
+        List<Object> allSpatialPoints=points_rdd.collect();
+        List<String> allSpatialFeatures=spatial_feature_rdd.collect();
         HashMap<Object, HashMap<String,Integer>> neighbour_count_map=new HashMap<>();
         HashMap<Object, HashMap<String,Double>> label_set=new HashMap<>();
 //        JavaPairRDD<Spatial_Point, HashMap<Spatial_Feature,Double>> label_set_rdd;
-        for (Object p:points_rdd.collect()) {
+        for (Object p:allSpatialPoints) {
             HashMap<String,Integer> map_neigh=new HashMap<>();
             HashMap<String,Double> map_label=new HashMap<>();
-            for (String f:spatial_feature_rdd.collect())
+            for (String f:allSpatialFeatures)
             {
                 map_neigh.put(f,0);
                 map_label.put(f,0D);
@@ -110,22 +114,41 @@ public class FractionScore {
             }
             neighbour_count_map.put(p,map_neigh);
             label_set.put(p,map_label);
+//            System.out.println("Neighbour map" + neighbour_count_map.get(p).size());
         }
+//        System.out.println(neighbour_count_map.size());
+//        for (Object p:points_rdd.collect()) {
+//
+//            System.out.println("Neighbour map" + neighbour_count_map.get(p).size());
+//        }
+//        System.out.println("points");
+//        for (Object p:allSpatialPoints) {
+//            System.out.println(p);
+//        }
+//        System.out.println("neigh");
+//        for(Map.Entry m:neighbour_count_map.entrySet())
+//        {
+//            System.out.println(m.getKey());
+//        }
 
-
-        for (Object o:points_rdd.collect()) {
+        for (Object o:allSpatialPoints) {
             LinkedList<Object> list_of_points_in_d=find_points_in_dist_d(dist_thresh,o,neighbour_count_map);
+//            System.out.println("val="+list_of_points_in_d.size());
             for(Object o_dash:list_of_points_in_d)
             {
                 String str=o_dash.event_type;
-                neighbour_count_map.get(o).replace(str,neighbour_count_map.get(o).get(str),neighbour_count_map.get(o).get(str)+1);
+                int val=neighbour_count_map.get(o).get(str);
+                neighbour_count_map.get(o).replace(str,val+1);
             }
 
 
             for(Object o_dash:list_of_points_in_d)
             {
                 String str=o.event_type;
+                int v=neighbour_count_map.get(o).get(str);
                 double obj=1D/neighbour_count_map.get(o).get(str);
+//                System.out.println(v+" "+obj);
+
                 label_set.get(o_dash).replace(str,label_set.get(o_dash).get(str),label_set.get(o_dash).get(str)+obj);
                 if(label_set.get(o_dash).get(str)>1)
                 {
@@ -138,6 +161,26 @@ public class FractionScore {
 
     }
 
+    public static Double FractionAggregation(ArrayList<String> labelSet, Object o, JavaRDD<Object> allSpatialObjects ,HashMap<Object, HashMap<String,Double>> label_set_rdd)
+    {
+        String label=o.event_type;
+
+        double labelSetValue=Double.MAX_VALUE;
+
+        //iterating over the labelSet,i.e., the candidate co-location
+        for(String str:labelSet)
+        {
+            if(!str.equals(label))
+            {
+                double val=label_set_rdd.get(o).get(str);
+                if(val<labelSetValue)
+                {
+                    labelSetValue=val;
+                }
+            }
+        }
+        return labelSetValue;
+    }
     public static Object create_Object(String line)
     {
         String values [] = line.split(" ");
@@ -157,7 +200,7 @@ public class FractionScore {
         JavaRDD<String> lines = sc.textFile("data.txt");
 
         /*creating objects using create_Object function*/
-        JavaRDD<Object> allSpatialObjects = lines.map(x -> create_Object(x));
+        JavaRDD<Object> allSpatialObjects = lines.map(FractionScore::create_Object);
         /*
         printing all objects
          */
@@ -183,7 +226,7 @@ public class FractionScore {
             }
         });
 //        System.out.println("LABELS RDD");
-        HashMap<Object, HashMap<String,Double>> label_set_rdd=FractionComputation(allSpatialObjects,allEventTypes,25);
+        HashMap<Object, HashMap<String,Double>> label_set_rdd=FractionComputation(allSpatialObjects,allEventTypes,0.2);
 //        label_set_rdd.foreach(new VoidFunction<Tuple2<Spatial_Point, HashMap<String, Double>>>() {
 //            @Override
 //            public void call(Tuple2<Spatial_Point, HashMap<String, Double>> spatial_pointHashMapTuple2) throws Exception {
@@ -197,6 +240,16 @@ public class FractionScore {
 //                }
 //            }
 //        });
+        label_set_rdd.forEach(new BiConsumer<Object, HashMap<String, Double>>() {
+            @Override
+            public void accept(Object object, HashMap<String, Double> stringDoubleHashMap) {
+                System.out.println(object.event_type+object.instance_id);
+                for(Map.Entry<String,Double> m:stringDoubleHashMap.entrySet())
+                {
+                    System.out.println("\t"+m.getKey()+" "+m.getValue());
+                }
+            }
+        });
 
 
     }
